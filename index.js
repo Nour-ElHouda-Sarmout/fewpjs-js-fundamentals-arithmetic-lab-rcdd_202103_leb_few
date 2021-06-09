4372486cc5f9989498c2f3cb237ae94bccb302dc
Nour El Houda Sarmout <nourelhoudasarmout@gmail.com>
2021-06-09 10:41:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 let num1 = 31;
 let num2 = 2;
+let num3 = 16;
+let num4 = 6;
 let multiply = num1 * num2;
 let random = Math.ceil(Math.random());
-let mod = 16 % 6;
+let mod = num3 % num4;
 let max = Math.max(1, 2, 3, 20);
